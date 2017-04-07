b784c38fb3193b005dd1148bc75626750655f1a4
Ben Davis <bendavisnc@gmail.com>
2017-04-07 19:01:01 -0400

    friday p2

--- resources/public/js/cljsout/ccboard/client/piece_movement.cljs
+++
@@ -1,1 +0,0 @@
-(ns ccboard.client.piece-movement)
--- resources/public/js/cljsout/ccboard/client/piece_movement.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.9.494 {}
-goog.provide('ccboard.client.piece_movement');
-goog.require('cljs.core');
-
-//# sourceMappingURL=piece_movement.js.map
